c732333603bbf0c09ef6d77f35b4d752b9ec76be
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-07 21:04:24 -0700

    using pymongo instead of flask-mongo

--- apiapp/main/routes.py
+++ apiapp/main/routes.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 import os
 import pymongo
 
-from apiapp.extensions import mongo
-
 main = Blueprint('main', __name__)
 
 load_dotenv()
@@ -12,4 +10,15 @@ MONGO_URI = os.getenv('MONGO_URI')
 
 @main.route('/')
 def index():
+    return render_template('index.html')
+
+@main.route('/gen_api<id>')
+def index(id):
+    mongodb_client = pymongo.MongoClient(MONGO_URI)
+    db = mongodb_client["translatordb"]
+    col = db["api_keys"]
+    specs = {
+            "key" : id,
+        }
+    result = col.update_one({}, {'$set':specs}, True)
     return render_template('index.html')
