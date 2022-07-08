6219c717eaf10cc8ad7171c59d8202de171df13e
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-07 20:55:00 -0700

    test

--- apiapp/app.py
+++ apiapp/app.py
@@ -1,19 +1,13 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
 
 from .main.routes import main
 from .extensions import mongo
 
-load_dotenv()
-MONGO_URI = os.getenv('MONGO_URI')
 
 def create_app():
     app = Flask(__name__)
 
     app.config['MONGO_URI'] = MONGO_URI
-
-    mongo.init_app(app)
 
     app.register_blueprint(main)
     return app
--- apiapp/main/routes.py
+++ apiapp/main/routes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, render_template
+from dotenv import load_dotenv
+import os
+import pymongo
 
 from apiapp.extensions import mongo
 
 main = Blueprint('main', __name__)
 
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+
 @main.route('/')
 def index():
-    api_keys = mongo.db.api_keys
-    api_keys.insert_one({'text' : 10})
     return render_template('index.html')
