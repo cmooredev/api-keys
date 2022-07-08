d99025bf9a329751aa0deab1fe0c8762cb2d2669
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-07 20:34:58 -0700

    test db connection

--- apiapp/main/routes.py
+++ apiapp/main/routes.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
 
+from .extensions import mongo
+
+api_keys = mongo.db.api_keys
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    api_keys.insert_one({'text' : 10})
     return render_template('index.html')
