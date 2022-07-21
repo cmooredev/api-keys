89f0bcefa5a26233c04804942c6472e55cf7264b
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-21 14:03:00 -0700

    test modal

---
+++ apiapp/static/assets/js/app.js
@@ -0,0 +1,7 @@
+var myModal = document.getElementById('serverModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+s
