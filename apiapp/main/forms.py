b97e40ae471c16f8e7a7e9219f373fe55070c1e6
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-22 09:15:56 -0700

    test for validation

--- apiapp/main/forms.py
+++ apiapp/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms.fields.html5 import IntegerField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    server_id = StringField('server_id', validators=[DataRequired()])
+    server_id = IntegerField('Server ID', validators=[DataRequired()])
