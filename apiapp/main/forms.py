d2849b82c223c082376f346980911afc03e18bc0
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-22 09:47:09 -0700

    test for validation

--- apiapp/main/forms.py
+++ apiapp/main/forms.py
@@ -3,4 +3,12 @@ from wtforms.fields import IntegerField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    server_id = IntegerField('Server ID', validators=[DataRequired()])
+    server_id = IntegerField('Server ID',
+        validators=[DataRequired()]),
+        validators.Regexp('^([\s\d]+)$', message='Numbers only')
+
+    def validate_server(form, field):
+        if len(field.data) < 18:
+            raise ValidationError("Server ID too short")
+        if len(field.data) > 17:
+            raise ValidationError("Server ID too long")
