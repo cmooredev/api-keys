20831c42d853ee532e40ab892b8b288601a11586
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-22 10:02:49 -0700

    fix imports

--- apiapp/main/forms.py
+++ apiapp/main/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp
 
 class MyForm(FlaskForm):
-    server_id = StringField('Server ID', [validators.Regexp('^([\s\d]+)$', message='Numbers only')])
+    server_id = StringField('Server ID', validators=[DataRequired(),
+                Regexp('^([\s\d]+)$', message='Numbers only')
+                ])
 
     def validate_server(form, field):
         if len(field.data) < 18:
