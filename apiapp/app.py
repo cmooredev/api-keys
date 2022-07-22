75161c2339ff1df4a1eb8a9001d830e1d2bc5bc1
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-07-22 07:37:35 -0700

    updated comments

--- apiapp/app.py
+++ apiapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+#import talisman to force https
 from flask_talisman import Talisman
 
 from .main.routes import main
@@ -6,9 +7,8 @@ from .main.routes import main
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(main)
+    #wrap app with Talisman to force https
     Talisman(app, content_security_policy=None)
     return app
-
-
 
 app = create_app()
