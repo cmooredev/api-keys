6c8304291905dd87f733c2681037275e2a4dff82
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-08-03 06:12:53 -0700

    update length

--- apiapp/main/forms.py
+++ apiapp/main/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired, Regexp, Length
 class MyForm(FlaskForm):
     server_id = StringField('Server ID', validators=[DataRequired(),
                 Regexp(regex=r'^[0-9]*$', message='Numbers only'),
-                Length(min=18, max=18, message="Server IDs are usually 18 digits.")
+                Length(min=17, max=20, message="Server IDs are usually 18-19 digits.")
                 ])
